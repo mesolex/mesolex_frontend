d54ac9e33dae80adbc27dc0c12550e3ec444b4d1
Neil Ashton <neil.m.ashton@gmail.com>
2021-08-15 09:58:05 -0400

    Add senses

--- src/components/results/azz.tsx
+++ src/components/results/azz.tsx
@@ -50,7 +50,60 @@ const Azz = ({
             {item.grammar_groups.map(({ part_of_speech }) => part_of_speech).join(', ')}
           </>
         }
+
+        {
+          !isEmpty(item.categories)
+          && <>
+            &nbsp;|&nbsp;
+            <i>Campo semántico: </i>
+            {item.categories.join(', ')}
+          </>
+        }
+
+        {
+          /* TODO: get <vnawa> elements working right */
+          !isEmpty(item.non_native_etymologies)
+          && <>
+            &nbsp;|&nbsp;
+            <i>Palabras con elementos no de náhuat: </i>
+            {item.non_native_etymologies.map(({ value }) => value).join(', ')}
+          </>
+        }
       </Card.Header>
+      <Card.Body>
+        <ol className="search-results-list">
+          {
+            item.senses.map((sense) => (
+              <li>
+                <Card.Text>
+                  <p>
+                    {sense.sense}
+                    {
+                      !isEmpty(sense.ostentives)
+                      && <>
+                        &nbsp;({sense.ostentives.join(' ')})
+                      </>
+                    }
+                  </p>
+                </Card.Text>
+                {
+                  sense.examples.map((example) => (
+                    <blockquote className="card-text example font-weight-light">
+                      <p>
+                        {example.original.text}
+                        {
+                          !isEmpty(example.translation)
+                          && <>&nbsp;<i>{example.translation.text}</i></>
+                        }
+                      </p>
+                    </blockquote>
+                  ))
+                }
+              </li>
+            ))
+          }
+        </ol>
+      </Card.Body>
     </Card>
   );
 };
